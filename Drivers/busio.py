66bdbe786b4422155863ea2698e8bf7ba909f05b
Jon Wakefield <67289517+JonWakefield@users.noreply.github.com>
2023-03-06 12:33:33 -0600

    Update busio.py

--- Drivers/busio.py
+++ Drivers/busio.py
@@ -14,41 +14,7 @@
                     > Chip is the RP2040
 '''
 
-
-
-class ContextManaged:
-    """An object that automatically deinitializes hardware with a context manager."""
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        self.deinit()
-
-    # pylint: disable=no-self-use
-    def deinit(self):
-        """Free any hardware used by the object."""
-        return
-
-class Lockable(ContextManaged):
-    """An object that must be locked to prevent collisions on a microcontroller resource."""
-
-    _locked = False
-
-    def try_lock(self):
-        """Attempt to grab the lock. Return True on success, False if the lock is already taken."""
-        if self._locked:
-            return False
-        self._locked = True
-        return True
-
-    def unlock(self):
-        """Release the lock so others may use the resource."""
-        if self._locked:
-            self._locked = False
-        else:
-            raise ValueError("Not locked")
-
+from adafruit_blinka import Enum, Lockable
 
 # Set our Chip to the RP2040
 CHIP_ID = "RP2040"
